7ad7070a740f7fcb9ceb4c771a0c70ca840c5134
Michael Huitt <michael.c.huitt@gmail.com>
2025-11-09 01:11:15 -0500

    add Conn class and change auto_scp to accept Conn object

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from sys import argv
 
 CONF_PATH = "settings.conf"
 
+class Conn:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+
+        if port is None:
+            self.port = 22
+
 def get_conf(filepath: str) -> dict:
     try:
         conf_dict = {} 
@@ -17,15 +24,19 @@ def get_conf(filepath: str) -> dict:
     except FileNotFoundError:
         print("The config file does not exist")
 
-def auto_scp(client_path: str, host_path: str, hostname: str, port: str):
+def auto_scp(client_path: str, host_path: str, SSH_Conn: Conn):
     pass
 
 def main():
     try: 
-        conf_dict = get_conf(CONF_PATH)
         if len(argv) < 3:
             raise ValueError("Usage: main.py <client path> <host path>")
-    
+        
+        conf = get_conf(CONF_PATH)
+        SSH_Conn = Conn(conf.get("hostname"), conf.get("port"))  
+        client_path = argv[1]
+        host_path = argv[2]
+
     except Exception as e:
         print("error: ", repr(e))
 
