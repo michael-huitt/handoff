a735e94361cc46d2dd6320ef341991d4e9a3a997
Michael Huitt <michael.c.huitt@gmail.com>
2025-11-09 19:53:43 -0500

    add comments for documentation

--- handoff.py
+++ handoff.py
@@ -6,6 +6,7 @@ from shutil import rmtree
 
 CONF_PATH = "settings.conf"
 
+##Fancy struct that sets the port to 22 if not specified, represents generic SSH connection info.
 class Conn:
     def __init__(self, user, hostname, port):
         self.user = user 
@@ -15,6 +16,8 @@ class Conn:
         if port is None:
             self.port = 22
 
+##Read in the configuration file and return a dictionary where the prefix and postfix of '=' are
+##the key value pair.
 def get_conf(filepath: str) -> dict:
     try:
         conf_dict = {} 
@@ -32,6 +35,9 @@ def get_conf(filepath: str) -> dict:
     except FileNotFoundError:
         print("The config file does not exist")
 
+
+##Run the scp command given the client path, host path, a Conn object, and a list of scp flags. Returns a string that
+##gives details on the operation, first a generic "success" message followed by anything scp might return.
 def auto_scp(client_path: str, host_path: str, SSH_Conn: Conn, flags: list) -> str:
     try:
         #combine the user@hostname and path into one argument for run() 
@@ -40,7 +46,7 @@ def auto_scp(client_path: str, host_path: str, SSH_Conn: Conn, flags: list) -> s
         
         else:
             destination_arg = SSH_Conn.hostname + ":" + host_path #so that scp defaults to the current session's user 
-
+                                                                  #if not specified
         Result = run(['scp','-P', SSH_Conn.port, *flags, client_path, destination_arg],
                     capture_output = True, text = True)
         
@@ -48,7 +54,7 @@ def auto_scp(client_path: str, host_path: str, SSH_Conn: Conn, flags: list) -> s
             raise Exception(f"scp exited with code ({Result.returncode}): {Result.stderr.strip()}")
 
         else:
-            if Result.stderr != '': 
+            if Result.stderr != '': #returned on non-verbose scp success 
                 return (f"Transfer succesful {client_path} -> {SSH_Conn.user}@{SSH_Conn.hostname}:{host_path}"
                         f"\n{Result.stderr}")
             else:
@@ -57,11 +63,12 @@ def auto_scp(client_path: str, host_path: str, SSH_Conn: Conn, flags: list) -> s
     except Exception as e:
         print(f"auto_scp error: {e}")
 
-def handle_flags(flags: list, client_path: str) -> int:
+##Iterate through a list of flags and operate on the client path depending on the flags.
+def handle_flags(flags: list, client_path: str):
     try:
         for flag in flags:
             if flag == "-d":
-                if isdir(client_path):
+                if isdir(client_path) 
                     rmtree(client_path)
 
                 else: 
